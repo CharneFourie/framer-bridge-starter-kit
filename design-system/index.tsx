264ff18e690daeeda7dbcf5a62a20597441e4e27
Charne Fourie <charne.fourie@accenture.com>
2020-03-09 12:12:55 +0200

    Actual new button

--- design-system/index.tsx
+++ design-system/index.tsx
@@ -1,4 +1,5 @@
 export { Button, Props as ButtonProps } from "./components/Button"
+export { ButtonMain, Props as ButtonMainProps } from "./components/ButtonMain"
 export { Input, Props as InputProps } from "./components/Input"
 export { Toggle, Props as ToggleProps } from "./components/Toggle"
 export { Tooltip, Props as TooltipProps } from "./components/Tooltip"
