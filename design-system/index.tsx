13d8ef87b3cab2bc203791f48029bebfce2d4922
Charne Fourie <charne.fourie@accenture.com>
2020-03-09 12:25:18 +0200

    do it

--- design-system/index.tsx
+++ design-system/index.tsx
@@ -1,5 +1,4 @@
 export { Button, Props as ButtonProps } from "./components/Button"
-export { ButtonMain, Props as ButtonMainProps } from "./components/ButtonMain"
 export { Input, Props as InputProps } from "./components/Input"
 export { Toggle, Props as ToggleProps } from "./components/Toggle"
 export { Tooltip, Props as TooltipProps } from "./components/Tooltip"
